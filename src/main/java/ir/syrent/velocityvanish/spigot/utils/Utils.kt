43cf651b38e4826106cf170ee45bbf1407045ff8
Syrent <Syrent2356@gmail.com>
2022-12-13 18:03:03 +0330

    Spigot | Some actionbar related changes.

--- src/main/java/ir/syrent/velocityvanish/spigot/utils/Utils.kt
+++ src/main/java/ir/syrent/velocityvanish/spigot/utils/Utils.kt
@@ -6,23 +6,40 @@ import ir.syrent.velocityvanish.spigot.ruom.string.CharAnimation
 import ir.syrent.velocityvanish.spigot.storage.Message
 import ir.syrent.velocityvanish.spigot.storage.Settings
 import ir.syrent.velocityvanish.utils.TextReplacement
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
+import org.bukkit.scheduler.BukkitRunnable
 
 object Utils {
 
+    // TODO: Crate new Actionbar class
+
     val actionbarPlayers = mutableSetOf<Player>()
+    val charAnimation = CharAnimation(CharAnimation.Style.SQUARE_BLOCK)
+    var lastChar = ""
+
+    init {
+        Ruom.runSync({
+            lastChar = charAnimation.get().toString()
+        }, 0, 30)
+    }
 
     fun sendReportsActionbar(player: Player) {
         if (actionbarPlayers.contains(player)) return
 
-        val charAnimation = CharAnimation(CharAnimation.Style.SQUARE_BLOCK)
-
         if (Settings.actionbar && player.hasPermission("velocityvanish.admin.actionbar")) {
-            Ruom.runSync({
-                if (!VelocityVanishSpigot.instance.vanishedNames.contains(player.name)) return@runSync
-                player.sendActionbar(Message.VANISH_ACTIONBAR, TextReplacement("animation", charAnimation.get().toString()))
-                actionbarPlayers.add(player)
-            }, 0, 20)
+            object : BukkitRunnable() {
+                override fun run() {
+                    if (Bukkit.getPlayer(player.uniqueId) == null) {
+                        cancel()
+                        return
+                    }
+
+                    if (!VelocityVanishSpigot.instance.vanishedNames.contains(player.name)) return
+
+                    player.sendActionbar(Message.VANISH_ACTIONBAR, TextReplacement("animation", lastChar))
+                }
+            }.runTaskTimer(VelocityVanishSpigot.instance, 0, 20)
         }
     }
 

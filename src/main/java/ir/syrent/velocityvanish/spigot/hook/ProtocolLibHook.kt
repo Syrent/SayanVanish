250cf3e7b0559e3ba771ff8d01d4fbdcd4883a65
Syrent <Syrent2356@Gmail.com>
2023-09-21 21:41:04 +0330

    Handle NoClassDefError for ProtocolLibHook initialization to fix Purpur problem on 1.19.4

--- src/main/java/ir/syrent/velocityvanish/spigot/hook/ProtocolLibHook.kt
+++ src/main/java/ir/syrent/velocityvanish/spigot/hook/ProtocolLibHook.kt
@@ -2,15 +2,19 @@ package ir.syrent.velocityvanish.spigot.hook
 
 import com.comphenix.protocol.ProtocolLibrary
 import com.comphenix.protocol.ProtocolManager
+import ir.syrent.velocityvanish.spigot.ruom.Ruom
 
 class ProtocolLibHook constructor(name: String) : Dependency(name) {
 
-    lateinit var protocolManager: ProtocolManager
-        private set
+    var protocolManager: ProtocolManager? = null
 
     init {
         if (exists) {
-            protocolManager = ProtocolLibrary.getProtocolManager()
+            try {
+                protocolManager = ProtocolLibrary.getProtocolManager()
+            } catch (_: NoClassDefFoundError) {
+                Ruom.warn("Could not initialize ProtocolLib hook. If you want to use its features you may need to upgrade your server and plugin to latest build.")
+            }
         }
     }
 

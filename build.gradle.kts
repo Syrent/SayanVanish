7b03c62b250a33b02d0f00bcf16b86ebed6e5635
Syrent <Syrent2356@Gmail.com>
2024-06-01 11:51:00 +0330

    chore: release candidate 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ description = "A modular vanish system for Minecraft servers"
 
 allprojects {
     group = "org.sayandev"
-    version = "1.0.0-rc.2"
+    version = "1.0.0-rc.3"
 
     plugins.apply("java-library")
     plugins.apply("maven-publish")

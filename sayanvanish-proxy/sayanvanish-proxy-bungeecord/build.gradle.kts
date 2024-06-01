6d25052a0f9947c5a86a57f0e9cd6c68768d0b9a
Syrent <Syrent2356@Gmail.com>
2024-06-01 19:46:42 +0330

    chore(proxy-bungeecord): use `plugin-yml` gradle plugin instead of `plugin.yml`

--- sayanvanish-proxy/sayanvanish-proxy-bungeecord/build.gradle.kts
+++ sayanvanish-proxy/sayanvanish-proxy-bungeecord/build.gradle.kts
@@ -1,6 +1,10 @@
 import org.sayandev.*
 import org.sayandev.applyShadowRelocation
 
+plugins {
+    id("net.minecrell.plugin-yml.bungee") version "0.6.0"
+}
+
 generateTemplate(Module.BUNGEECORD)
 
 repositories {
@@ -15,4 +19,14 @@ tasks {
     shadowJar {
         applyShadowRelocation(Module.BUNGEECORD)
     }
+}
+
+bungee {
+    name = rootProject.name
+    version = rootProject.version as String
+    description = rootProject.description
+
+    main = "${rootProject.group}.${findProperty("slug")!! as String}.bungeecord.${rootProject.name}"
+
+    author = findProperty("author")!! as String
 }

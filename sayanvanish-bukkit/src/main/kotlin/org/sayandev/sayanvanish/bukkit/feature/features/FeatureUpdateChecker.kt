a876a8442195f55ec915f2bfe2a60b5e849890d5
Syrent <Syrent2356@Gmail.com>
2024-06-02 11:43:58 +0330

    feat(bukkit): add update checker

---
+++ sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/feature/features/FeatureUpdateChecker.kt
@@ -0,0 +1,125 @@
+package org.sayandev.sayanvanish.bukkit.feature.features
+
+import org.bukkit.Bukkit
+import org.bukkit.command.CommandSender
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerJoinEvent
+import org.sayandev.sayanvanish.api.feature.RegisteredFeature
+import org.sayandev.sayanvanish.bukkit.api.event.BukkitUserUnVanishEvent
+import org.sayandev.sayanvanish.bukkit.feature.ListenedFeature
+import org.sayandev.sayanvanish.bukkit.utils.HangarUtils
+import org.sayandev.sayanvanish.bukkit.utils.VersionInfo
+import org.sayandev.stickynote.bukkit.log
+import org.sayandev.stickynote.bukkit.runAsync
+import org.sayandev.stickynote.bukkit.runSync
+import org.sayandev.stickynote.bukkit.utils.AdventureUtils.component
+import org.sayandev.stickynote.bukkit.utils.AdventureUtils.sendMessage
+import org.sayandev.stickynote.bukkit.warn
+import org.sayandev.stickynote.lib.kyori.adventure.text.Component
+import org.sayandev.stickynote.lib.spongepowered.configurate.objectmapping.ConfigSerializable
+
+@RegisteredFeature
+@ConfigSerializable
+class FeatureUpdateChecker(
+    val checkEveryXMinutes: Int = 60,
+    val notifyPermission: String = "sayanvanish.feature.updatechecker.notify",
+    val notifyOnJoin: Boolean = true,
+    val content: List<String> = listOf(
+        "<green>A new version of <white>SayanVanish</white> is available!",
+        "<aqua> - Latest release: <white><latest_release_name>",
+        "  <yellow>- <gray>Click to download: <blue><click:open_url:'<latest_release_url_paper>'>Paper</click> <gray>|</gray> <aqua><click:open_url:'<latest_release_url_velocity>'>Velocity</click> <gray>|</gray> <blue><click:open_url:'<latest_release_url_waterfall>'>Waterfall</click>",
+        "  <yellow>- <gray>Changelog: <gray><latest_release_changelog>",
+        "  <yellow>- <gray><click:open_url:'https://hangar.papermc.io/Syrent/SayanVanish/versions/<latest_release_name>'>Click to see full changelog",
+        "<aqua> - Latest snapshot: <white><latest_snapshot_name>",
+        "  <yellow>- <gray>Click to download: <blue><click:open_url:'<latest_snapshot_url_paper>'>Paper</click> <gray>|</gray> <aqua><click:open_url:'<latest_snapshot_url_velocity>'>Velocity</click> <gray>|</gray> <blue><click:open_url:'<latest_snapshot_url_waterfall>'>Waterfall</click>",
+        "  <yellow>- Changelog: <gray><latest_snapshot_changelog>",
+        "  <yellow>- <gray><click:open_url:'https://hangar.papermc.io/Syrent/SayanVanish/versions/<latest_snapshot_name>'>Click to see full changelog"
+    )
+) : ListenedFeature("update_checker") {
+
+    @Transient var latestRelease: VersionInfo? = null
+    @Transient var latestSnapshot: VersionInfo? = null
+
+    init {
+        runAsync({
+            if (!isActive()) return@runAsync
+            log("Checking for updates...")
+            HangarUtils.getLatestRelease().whenComplete { latestRelease, releaseError ->
+                releaseError?.printStackTrace()
+
+                log("Checked latest release")
+                this.latestRelease = latestRelease
+
+                HangarUtils.getLatestSnapshot().whenComplete { latestSnapshot, snapshotError ->
+                    snapshotError?.printStackTrace()
+
+                    log("Checked latest snapshot")
+                    this.latestSnapshot = latestSnapshot
+
+                    runSync {
+                        warn("1")
+                        send(Bukkit.getConsoleSender())
+                    }
+                }
+            }
+        }, 0, checkEveryXMinutes * 60 * 20L)
+    }
+
+    @EventHandler
+    private fun onVanish(event: PlayerJoinEvent) {
+        warn("2")
+        if (!isActive()) return
+        warn("3")
+        val player = event.player
+        warn("notifyOnJoin: ${notifyOnJoin}")
+        warn("player.hasPermission(notifyPermission): ${player.hasPermission(notifyPermission)}")
+        warn("latestRelease != null: ${latestRelease != null}")
+        warn("latestSnapshot != null: ${latestSnapshot != null}")
+        if (notifyOnJoin && player.hasPermission(notifyPermission) && latestRelease != null && latestSnapshot != null) {
+            warn("4")
+            send(player)
+        }
+    }
+
+    @EventHandler
+    private fun onUnVanish(event: BukkitUserUnVanishEvent) {
+        if (!isActive()) return
+        val user = event.user
+        user.sendActionbar(Component.empty())
+    }
+
+    private fun send(sender: CommandSender) {
+        if (latestRelease == null || latestSnapshot == null) return
+        for (line in content) {
+            sender.sendMessage(line
+                .replace("<latest_release_name>", latestRelease?.name ?: "Unknown")
+                .replace("<latest_release_url_paper>", latestRelease?.downloads?.PAPER?.downloadUrl() ?: "https://hangar.papermc.io/Syrent/SayanVanish")
+                .replace("<latest_release_url_velocity>", latestRelease?.downloads?.VELOCITY?.downloadUrl() ?: "https://hangar.papermc.io/Syrent/SayanVanish")
+                .replace("<latest_release_url_waterfall>", latestRelease?.downloads?.WATERFALL?.downloadUrl() ?: "https://hangar.papermc.io/Syrent/SayanVanish")
+                .replace("<latest_release_changelog>", shortDescription(latestRelease?.description) ?: "Unknown")
+                .replace("<latest_snapshot_name>", latestSnapshot?.name ?: "Unknown")
+                .replace("<latest_snapshot_url_paper>", latestSnapshot?.downloads?.PAPER?.downloadUrl() ?: "https://hangar.papermc.io/Syrent/SayanVanish")
+                .replace("<latest_snapshot_url_velocity>", latestSnapshot?.downloads?.VELOCITY?.downloadUrl() ?: "https://hangar.papermc.io/Syrent/SayanVanish")
+                .replace("<latest_snapshot_url_waterfall>", latestSnapshot?.downloads?.WATERFALL?.downloadUrl() ?: "https://hangar.papermc.io/Syrent/SayanVanish")
+                .replace("<latest_snapshot_changelog>", shortDescription(latestSnapshot?.description) ?: "Unknown")
+                .component()
+            )
+        }
+    }
+
+    private fun shortDescription(description: String?): String? {
+        if (description == null) return null
+        return if (description.length > 45) {
+            val words = description.split(" ")
+            var result = ""
+            for (word in words) {
+                if (result.length + word.length > 45) break
+                result += "$word "
+            }
+            result.trim() + "..."
+        } else {
+            description
+        }
+    }
+
+}
---
+++ sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/utils/HangarUtils.kt
@@ -0,0 +1,74 @@
+package org.sayandev.sayanvanish.bukkit.utils
+
+import com.google.gson.annotations.SerializedName
+import org.sayandev.stickynote.lib.gson.Gson
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import java.util.concurrent.CompletableFuture
+
+object HangarUtils {
+    private val gson = Gson()
+
+    fun getLatestRelease(): CompletableFuture<VersionInfo> {
+        return get("https://hangar.papermc.io/api/v1/projects/sayanvanish/latestrelease")
+    }
+
+    fun getLatestSnapshot(): CompletableFuture<VersionInfo> {
+        return get("https://hangar.papermc.io/api/v1/projects/sayanvanish/latest?channel=Snapshot")
+    }
+
+    private fun get(urlString: String): CompletableFuture<VersionInfo> {
+        val future = CompletableFuture<VersionInfo>()
+
+        CompletableFuture.runAsync {
+            val url = URL(urlString)
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+            connection.setRequestProperty("User-Agent", USER_AGENT)
+            connection.setRequestProperty("Accept", "text/plain")
+
+            val version = BufferedReader(InputStreamReader(connection.inputStream)).use { it.readLine() }
+
+            val versionUrl = URL("$HANGAR_API_URL/$version")
+            val versionConnection = versionUrl.openConnection() as HttpURLConnection
+            versionConnection.requestMethod = "GET"
+            versionConnection.setRequestProperty("User-Agent", USER_AGENT)
+            versionConnection.setRequestProperty("Accept", "application/json")
+
+            val versionInfo = BufferedReader(InputStreamReader(versionConnection.inputStream)).use { reader ->
+                gson.fromJson(reader, VersionInfo::class.java)!!
+            }
+
+
+            future.complete(versionInfo)
+        }
+
+        return future
+    }
+
+    private const val HANGAR_API_URL = "https://hangar.papermc.io/api/v1/projects/sayanvanish/versions"
+    private const val USER_AGENT: String = "Mozilla/5.0"
+}
+
+data class VersionInfo(
+    val name: String,
+    val description: String,
+    val downloads: Downloads
+)
+
+data class Downloads(
+    @SerializedName("PAPER") val PAPER: PlatformInfo?,
+    @SerializedName("WATERFALL") val WATERFALL: PlatformInfo?,
+    @SerializedName("VELOCITY") val VELOCITY: PlatformInfo?
+)
+
+data class PlatformInfo(
+    val externalUrl: String?,
+    @SerializedName("downloadUrl") val downloadUrl: String?
+) {
+    fun downloadUrl(): String {
+        return externalUrl ?: downloadUrl ?: ""
+    }
+}

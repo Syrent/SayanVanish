fa66302839a722bdb5433131fd594c799520c46b
Syrent <Syrent2356@Gmail.com>
2024-06-10 15:41:49 +0330

    fix(bukkit): feature prevent spawner spawn (fix isPaper method)

--- sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/feature/features/prevent/FeaturePreventSpawnerSpawn.kt
+++ sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/feature/features/prevent/FeaturePreventSpawnerSpawn.kt
@@ -3,12 +3,14 @@ package org.sayandev.sayanvanish.bukkit.feature.features.prevent
 import com.destroystokyo.paper.event.entity.PreSpawnerSpawnEvent
 import org.bukkit.GameMode
 import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.SpawnerSpawnEvent
 import org.sayandev.sayanvanish.api.feature.RegisteredFeature
 import org.sayandev.sayanvanish.api.feature.category.FeatureCategories
 import org.sayandev.sayanvanish.bukkit.api.SayanVanishBukkitAPI.Companion.user
 import org.sayandev.sayanvanish.bukkit.feature.ListenedFeature
 import org.sayandev.stickynote.bukkit.StickyNote
 import org.sayandev.stickynote.bukkit.onlinePlayers
+import org.sayandev.stickynote.bukkit.warn
 import org.sayandev.stickynote.lib.spongepowered.configurate.objectmapping.ConfigSerializable
 import org.sayandev.stickynote.lib.xseries.ReflectionUtils
 import kotlin.collections.all
@@ -25,7 +27,7 @@ class FeaturePreventSpawnerSpawn: ListenedFeature("prevent_spawner_spawn", categ
     private fun onPreSpawn(event: PreSpawnerSpawnEvent) {
         if (!isActive()) return
         val nearPlayers = onlinePlayers
-            .filter { player -> player.world == event.spawnerLocation.world && player.location.distance(event.spawnerLocation) <= 256 && player.gameMode != GameMode.SPECTATOR }
+            .filter { player -> player.gameMode != GameMode.SPECTATOR && player.world == event.spawnerLocation.world && player.location.distance(event.spawnerLocation) <= 256 }
         val allIsVanished = nearPlayers.all { it.user()?.isVanished == true }
         if (allIsVanished) {
             event.isCancelled = true

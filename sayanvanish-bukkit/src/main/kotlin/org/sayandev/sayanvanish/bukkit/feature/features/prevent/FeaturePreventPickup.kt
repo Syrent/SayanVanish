1453ec6f870a193a91242611137dae26dae53afe
Syrent <Syrent2356@Gmail.com>
2024-06-21 15:35:17 +0330

    chore(bukkit): add a new `legacy_prevent_pickup` feature for 1.8 servers

--- sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/feature/features/prevent/FeaturePreventPickup.kt
+++ sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/feature/features/prevent/FeaturePreventPickup.kt
@@ -3,15 +3,19 @@ package org.sayandev.sayanvanish.bukkit.feature.features.prevent
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.EntityPickupItemEvent
+import org.bukkit.event.player.PlayerPickupItemEvent
 import org.sayandev.sayanvanish.api.feature.RegisteredFeature
 import org.sayandev.sayanvanish.api.feature.category.FeatureCategories
 import org.sayandev.sayanvanish.bukkit.api.SayanVanishBukkitAPI.Companion.user
 import org.sayandev.sayanvanish.bukkit.feature.ListenedFeature
+import org.sayandev.stickynote.bukkit.utils.ServerVersion
 import org.sayandev.stickynote.lib.spongepowered.configurate.objectmapping.ConfigSerializable
 
 @RegisteredFeature
 @ConfigSerializable
 class FeaturePreventPickup: ListenedFeature("prevent_pickup", category = FeatureCategories.PREVENTION) {
+
+    override var condition: Boolean = ServerVersion.supports(9)
 
     @EventHandler
     private fun onPickupItem(event: EntityPickupItemEvent) {
@@ -21,5 +25,21 @@ class FeaturePreventPickup: ListenedFeature("prevent_pickup", category = Feature
             event.isCancelled = true
         }
     }
+}
 
+@RegisteredFeature
+@ConfigSerializable
+class FeatureLegacyPreventPickup: ListenedFeature("legacy_prevent_pickup", category = FeatureCategories.PREVENTION) {
+
+    override var condition: Boolean = !ServerVersion.supports(9)
+
+    @EventHandler
+    @Suppress("DEPRECATION")
+    private fun onPickupItem(event: PlayerPickupItemEvent) {
+        if (!isActive()) return
+        val user = event.player.user() ?: return
+        if (user.isVanished) {
+            event.isCancelled = true
+        }
+    }
 }

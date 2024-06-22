ea1cc4336591ab1d2c6cd91ebf77bea98d11441c
Syrent <Syrent2356@Gmail.com>
2024-06-22 13:20:11 +0330

    feat(bukkit): add `bypass-char` for `prevent_chat` feature

--- sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/feature/features/prevent/FeaturePreventChat.kt
+++ sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/feature/features/prevent/FeaturePreventChat.kt
@@ -8,11 +8,14 @@ import org.sayandev.sayanvanish.bukkit.api.SayanVanishBukkitAPI.Companion.user
 import org.sayandev.sayanvanish.bukkit.config.language
 import org.sayandev.sayanvanish.bukkit.feature.ListenedFeature
 import org.sayandev.stickynote.bukkit.utils.AdventureUtils.component
+import org.sayandev.stickynote.lib.kyori.adventure.text.minimessage.tag.resolver.Placeholder
 import org.sayandev.stickynote.lib.spongepowered.configurate.objectmapping.ConfigSerializable
 
 @RegisteredFeature
 @ConfigSerializable
-class FeaturePreventChat: ListenedFeature("prevent_chat", category = FeatureCategories.PREVENTION) {
+class FeaturePreventChat(
+    val bypassChar: String = "!"
+): ListenedFeature("prevent_chat", category = FeatureCategories.PREVENTION) {
 
     @EventHandler
     private fun onPlayerChat(event: AsyncPlayerChatEvent) {
@@ -20,10 +23,10 @@ class FeaturePreventChat: ListenedFeature("prevent_chat", category = FeatureCate
         val user = event.player.user() ?: return
         if (!user.isVanished) return
         val message = event.message
-        if (message.startsWith("!")) {
-            event.message = message.removePrefix("!")
+        if (message.startsWith(bypassChar)) {
+            event.message = message.removePrefix(bypassChar)
         } else {
-            user.sendMessage(language.vanish.cantChatWhileVanished.component())
+            user.sendMessage(language.vanish.cantChatWhileVanished.component(Placeholder.unparsed("char", bypassChar)))
             event.isCancelled = true
         }
     }

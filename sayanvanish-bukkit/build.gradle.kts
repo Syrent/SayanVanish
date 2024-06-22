9ff90da8521e571121e5275f5e84d85e7eeaef18
Syrent <Syrent2356@Gmail.com>
2024-06-22 13:15:48 +0330

    build(run-task): add spark ro `downloadPlugins`

--- sayanvanish-bukkit/build.gradle.kts
+++ sayanvanish-bukkit/build.gradle.kts
@@ -48,6 +48,7 @@ tasks {
 
         downloadPlugins {
             url("https://download.luckperms.net/1539/bukkit/loader/LuckPerms-Bukkit-5.4.126.jar")
+            url("https://ci.lucko.me/job/spark/418/artifact/spark-bukkit/build/libs/spark-1.10.73-bukkit.jar")
 //            url("https://github.com/Insprill/custom-join-messages/releases/download/v17.5.0/custom-join-messages-17.5.0.jar")
             url("https://github.com/EssentialsX/Essentials/releases/download/2.20.1/EssentialsX-2.20.1.jar")
         }

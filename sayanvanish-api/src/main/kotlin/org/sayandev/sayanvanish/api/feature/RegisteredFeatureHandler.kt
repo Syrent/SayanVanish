b6d63cac1d9655f3f820a128ffa97f5042d6b87f
Syrent <Syrent2356@Gmail.com>
2024-06-28 17:58:35 +0330

    chore: add alternative method to load features on platforms like CarbonSpigot

--- sayanvanish-api/src/main/kotlin/org/sayandev/sayanvanish/api/feature/RegisteredFeatureHandler.kt
+++ sayanvanish-api/src/main/kotlin/org/sayandev/sayanvanish/api/feature/RegisteredFeatureHandler.kt
@@ -1,17 +1,32 @@
 package org.sayandev.sayanvanish.api.feature
 
+import org.sayandev.sayanvanish.api.Platform
 import org.sayandev.stickynote.lib.reflections.Reflections
+import java.io.IOException
+import java.net.URL
+import java.security.CodeSource
+import java.util.*
+import java.util.jar.JarEntry
+import java.util.jar.JarFile
+import kotlin.collections.ArrayList
 
 
 object RegisteredFeatureHandler {
 
     fun process() {
         val reflections = Reflections("org.sayandev.sayanvanish")
-        val annotatedClasses = reflections.getTypesAnnotatedWith(RegisteredFeature::class.java)
+        val annotatedClasses = if (reflections.getTypesAnnotatedWith(RegisteredFeature::class.java).isEmpty()) {
+            Platform.get().logger.warning("Couldn't load plugin features in your current server software, trying alternative method...")
+            getClassesInPackage(Platform.get(), "org.sayandev.sayanvanish")
+        } else {
+            reflections.getTypesAnnotatedWith(RegisteredFeature::class.java)
+        }
 
+        Platform.get().logger.info("Found ${annotatedClasses.size} features.")
         for (annotatedClass in annotatedClasses) {
             createNewInstance(annotatedClass)
         }
+        Platform.get().logger.info("Enabled ${Features.features.size} features.")
     }
 
     private fun createNewInstance(clazz: Class<*>) {
@@ -30,5 +45,46 @@ object RegisteredFeatureHandler {
         } catch (e: Exception) {
             e.printStackTrace()
         }
+    }
+
+    fun getClassesInPackage(plugin: Any, packageName: String): Collection<Class<*>> {
+        val classes: MutableCollection<Class<*>> = ArrayList()
+        val codeSource: CodeSource = plugin.javaClass.protectionDomain.codeSource
+        val resource: URL = codeSource.location
+        val relPath = packageName.replace('.', '/')
+        val resPath = resource.path.replace("%20", " ")
+        val jarPath = resPath.replaceFirst("[.]jar[!].*".toRegex(), ".jar").replaceFirst("file:", "")
+
+        val jarFile: JarFile = try {
+            JarFile(jarPath)
+        } catch (e: IOException) {
+            Platform.get().logger.severe("Tried to find plugin jar file to load features, but couldn't find it. Your server software doesn't support this behavior.")
+            return emptyList()
+        }
+
+        val entries: Enumeration<JarEntry> = jarFile.entries()
+
+        while (entries.hasMoreElements()) {
+            val entry: JarEntry = entries.nextElement()
+            val entryName: String = entry.name
+            if (entryName.endsWith(".class") && entryName.startsWith(relPath) && entryName.length > relPath.length + 1) {
+                val className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "")
+                try {
+                    val clazz = plugin.javaClass.classLoader.loadClass(className)
+                    if (clazz.isAnnotationPresent(RegisteredFeature::class.java)) {
+                        classes.add(clazz)
+                    }
+                } catch (_: NoClassDefFoundError) {
+                } catch (_: ClassNotFoundException) { }
+            }
+        }
+
+        try {
+            jarFile.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return classes
     }
 }

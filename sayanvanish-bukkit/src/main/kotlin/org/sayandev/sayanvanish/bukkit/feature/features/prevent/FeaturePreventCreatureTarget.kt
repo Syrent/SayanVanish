3ea651fda4c6f3be1545f45705c3b0b69de189ed
Syrent <Syrent2356@Gmail.com>
2024-06-29 10:14:55 +0330

    fix(bukkit/folia): NPE on vanish

--- sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/feature/features/prevent/FeaturePreventCreatureTarget.kt
+++ sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/feature/features/prevent/FeaturePreventCreatureTarget.kt
@@ -1,42 +1,17 @@
 package org.sayandev.sayanvanish.bukkit.feature.features.prevent
 
-import org.bukkit.entity.Creature
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.EntityTargetEvent
 import org.sayandev.sayanvanish.api.feature.RegisteredFeature
 import org.sayandev.sayanvanish.api.feature.category.FeatureCategories
 import org.sayandev.sayanvanish.bukkit.api.SayanVanishBukkitAPI
-import org.sayandev.sayanvanish.bukkit.api.event.BukkitUserVanishEvent
 import org.sayandev.sayanvanish.bukkit.feature.ListenedFeature
-import org.sayandev.stickynote.bukkit.StickyNote
 import org.sayandev.stickynote.lib.spongepowered.configurate.objectmapping.ConfigSerializable
 
 @RegisteredFeature
 @ConfigSerializable
 class FeaturePreventCreatureTarget: ListenedFeature("prevent_creature_target", category = FeatureCategories.PREVENTION) {
-
-    @EventHandler
-    private fun onVanish(event: BukkitUserVanishEvent) {
-        if (!isActive()) return
-        val user = event.user
-        val player = user.player() ?: return
-        if (StickyNote.isFolia()) {
-            // TODO: Creature modification cannot be off region thread
-            player.world.entities
-                .filterIsInstance<Creature>()
-                .forEach { creature ->
-                    if (creature.target?.uniqueId == player.uniqueId) {
-                        creature.target = null
-                    }
-                }
-        } else {
-            player.world.entities
-                .filterIsInstance<Creature>()
-                .filter { mob -> player.uniqueId == mob.target?.uniqueId }
-                .forEach { mob -> mob.target = null }
-        }
-    }
 
     @EventHandler
     private fun onEntityTarget(event: EntityTargetEvent) {

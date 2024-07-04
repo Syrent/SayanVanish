4cef5b51b7267f24d70b1cf54ec9805fcfa29496
Syrent <Syrent2356@Gmail.com>
2024-07-04 17:16:08 +0330

    build(deps): update stickynote (fix command NPE on 1.21)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("io.papermc.hangar-publish-plugin") version "0.1.2"
     id("com.modrinth.minotaur") version "2.8.7"
-    id("org.sayandev.stickynote") version "1.1.16"
+    id("org.sayandev.stickynote") version "1.1.17"
 }
 
 val slug = findProperty("slug")!! as String

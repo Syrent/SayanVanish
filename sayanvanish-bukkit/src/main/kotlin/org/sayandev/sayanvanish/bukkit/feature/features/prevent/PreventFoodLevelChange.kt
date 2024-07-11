aac49b29b01a0f8e35bf9991643a7b51cc3c3e76
Syrent <Syrent2356@Gmail.com>
2024-07-11 19:11:41 +0330

    feat(bukkit): add `prevent_food_level_change` feature

---
+++ sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/feature/features/prevent/PreventFoodLevelChange.kt
@@ -0,0 +1,26 @@
+package org.sayandev.sayanvanish.bukkit.feature.features.prevent
+
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.FoodLevelChangeEvent
+import org.sayandev.sayanvanish.api.feature.RegisteredFeature
+import org.sayandev.sayanvanish.api.feature.category.FeatureCategories
+import org.sayandev.sayanvanish.bukkit.api.SayanVanishBukkitAPI.Companion.user
+import org.sayandev.sayanvanish.bukkit.feature.ListenedFeature
+import org.sayandev.stickynote.lib.spongepowered.configurate.objectmapping.ConfigSerializable
+
+@RegisteredFeature
+@ConfigSerializable
+class PreventFoodLevelChange: ListenedFeature("prevent_food_level_change", category = FeatureCategories.PREVENTION) {
+
+    @EventHandler
+    private fun onBlockBreak(event: FoodLevelChangeEvent) {
+        if (!isActive()) return
+        val player = event.entity as? Player ?: return
+        val user = player.user() ?: return
+        if (user.isVanished) {
+            event.isCancelled = true
+        }
+    }
+
+}

d4a087796e9056ba73fd2b7529b21434a9ce819e
syrent <syrent2356@gmail.com>
2024-11-08 06:51:25 +0330

    feat: add `Feature#loadAndRegister` for external use

--- sayanvanish-api/src/main/kotlin/org/sayandev/sayanvanish/api/feature/Feature.kt
+++ sayanvanish-api/src/main/kotlin/org/sayandev/sayanvanish/api/feature/Feature.kt
@@ -53,6 +53,10 @@ abstract class Feature(
         Platform.get().logger.warning("the feature '$id' is critical and currently disabled. We strongly recommend re-enabling it to avoid potential unexpected behavior. (path: ${directory(category).path}/${id}.yml)")
     }
 
+    fun loadAndRegister() {
+        loadAndRegister(this)
+    }
+
     companion object {
         fun directory(category: FeatureCategories) =
             when (category.directory) {
@@ -66,20 +70,31 @@ abstract class Feature(
 
         fun createFromConfig(type: Class<out Feature>): Feature {
             val freshInstance = type.getDeclaredConstructor().newInstance()
-            val category = freshInstance.category
+            return createFromInstance(freshInstance)
+        }
+
+        fun createFromInstance(feature: Feature): Feature {
+            val category = feature.category
             val instance = getConfigFromFile(File(
                 if (category.directory == null) {
                     File(Platform.get().rootDirectory, "features")
                 } else {
                     File(File(Platform.get().rootDirectory, "features"), category.directory)
-                }, "${freshInstance.id}.yml"),
-                freshInstance.additionalSerializers)?.get(type) ?: freshInstance
+                }, "${feature.id}.yml"),
+                feature.additionalSerializers)?.get(feature::class.java) ?: feature
             if (instance.enabled) {
                 instance.enable()
             } else {
                 instance.disable()
             }
             return instance
+        }
+
+        @JvmStatic
+        fun loadAndRegister(feature: Feature) {
+            createFromInstance(feature)
+            feature.load()
+            Features.addFeature(feature)
         }
     }
 }

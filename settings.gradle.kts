3f4198262c5dfcb74ebe5cbb2c9ec65fbb71ff6e
syrent <syrent2356@gmail.com>
2024-11-08 07:00:07 +0330

    feat: add 1.21.3 support

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.sayandev.stickynote.settings") version "1.7.94"
+    id("org.sayandev.stickynote.settings") version "1.7.95"
 }
 
 rootProject.name = "SayanVanish"

69597f6fa59fe6b7dd2c12653bbf1a0b073541bd
syrent <syrent2356@gmail.com>
2024-11-18 14:25:54 +0330

    build: update stickynote to 1.7.115 (dependency download/load improvements)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 
 plugins {
-    id("org.sayandev.stickynote.settings") version "1.7.113"
+    id("org.sayandev.stickynote.settings") version "1.7.115"
 }
 
 rootProject.name = "SayanVanish"

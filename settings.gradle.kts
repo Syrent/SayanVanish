a8605a3c5d27c63d4a1de6408b408ab12045b5f4
Syrent <syrent2356@gmail.com>
2025-02-12 23:13:30 +0330

    chore: remove overriden feature enable option from removed config system

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
-    id("org.sayandev.stickynote.settings") version "1.8.9.51"
+    id("org.sayandev.stickynote.settings") version "1.8.9.52"
 }
 
 rootProject.name = "SayanVanish"

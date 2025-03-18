0f3cfdf9363745dca9c0c9371110834037dd0f29
Syrent <syrent2356@gmail.com>
2025-03-18 17:16:28 +0330

    !EXPERIMENTAL! build: update stickynote to 1.8.9.129

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
-    id("org.sayandev.stickynote.settings") version "1.8.9.128"
+    id("org.sayandev.stickynote.settings") version "1.8.9.129"
 }
 
 rootProject.name = "SayanVanish"

d176857c28e8a7b214635caf244fd5401511abeb
Syrent <syrent2356@gmail.com>
2025-03-22 20:25:58 +0330

    feat(bukkit): add `priority` variable for `prevent_chat` feature

--- sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/feature/features/prevent/FeaturePreventChat.kt
+++ sayanvanish-bukkit/src/main/kotlin/org/sayandev/sayanvanish/bukkit/feature/features/prevent/FeaturePreventChat.kt
@@ -1,8 +1,10 @@
 package org.sayandev.sayanvanish.bukkit.feature.features.prevent
 
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
-import org.bukkit.event.EventHandler
+import org.bukkit.Bukkit
+import org.bukkit.event.Event
 import org.bukkit.event.EventPriority
+import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
 import org.sayandev.sayanvanish.api.feature.Configurable
 import org.sayandev.sayanvanish.api.feature.RegisteredFeature
@@ -10,6 +12,8 @@ import org.sayandev.sayanvanish.api.feature.category.FeatureCategories
 import org.sayandev.sayanvanish.bukkit.api.SayanVanishBukkitAPI.Companion.user
 import org.sayandev.sayanvanish.bukkit.config.language
 import org.sayandev.sayanvanish.bukkit.feature.ListenedFeature
+import org.sayandev.stickynote.bukkit.StickyNote
+import org.sayandev.stickynote.bukkit.plugin
 import org.spongepowered.configurate.objectmapping.ConfigSerializable
 import org.spongepowered.configurate.objectmapping.meta.Comment
 
@@ -17,23 +21,39 @@ import org.spongepowered.configurate.objectmapping.meta.Comment
 @ConfigSerializable
 class FeaturePreventChat(
     @Comment("The character that vanished players can use to bypass the chat prevention.")
-    @Configurable val bypassChar: String = "!"
+    @Configurable val bypassChar: String = "!",
+    @Comment("Requires server restart to apply.")
+    val priority: EventPriority = EventPriority.HIGH,
 ): ListenedFeature("prevent_chat", category = FeatureCategories.PREVENTION) {
 
-    @EventHandler(priority = EventPriority.HIGHEST)
-    @Suppress("DEPRECATION")
-    private fun onPlayerChat(event: AsyncPlayerChatEvent) {
-        if (event.isCancelled) return
-        val user = event.player.user() ?: return
-        if (!isActive(user)) return
-        if (!user.isVanished) return
-        val message = event.message
-        if (message.startsWith(bypassChar)) {
-            event.message = message.removePrefix(bypassChar)
-        } else {
-            user.sendComponent(language.vanish.cantChatWhileVanished, Placeholder.unparsed("char", bypassChar))
-            event.isCancelled = true
-        }
+    override fun enable() {
+        Bukkit.getPluginManager().registerEvent(
+            AsyncPlayerChatEvent::class.java,
+            this,
+            priority,
+            { listener: Listener, event: Event ->
+                if (event !is AsyncPlayerChatEvent) return@registerEvent
+                if (event.isCancelled) return@registerEvent
+                val user = event.player.user() ?: return@registerEvent
+                if (!isActive(user)) return@registerEvent
+                if (!user.isVanished) return@registerEvent
+                val message = event.message
+                if (message.startsWith(bypassChar)) {
+                    event.message = message.removePrefix(bypassChar)
+                } else {
+                    user.sendComponent(language.vanish.cantChatWhileVanished, Placeholder.unparsed("char", bypassChar))
+                    event.isCancelled = true
+                }
+            },
+            plugin,
+            false
+        )
+        super.enable()
+    }
+
+    override fun disable(reload: Boolean) {
+        StickyNote.unregisterListener(this)
+        super.disable(reload)
     }
 
 }

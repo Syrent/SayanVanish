40b5c25b7f724350c20fafd30f0dd43f92467911
Syren <syrent2356@gmail.com>
2025-07-16 16:07:40 +0330

    chore: add new todo

--- sayanvanish-api/src/main/kotlin/org/sayandev/sayanvanish/api/cache/CacheService.kt
+++ sayanvanish-api/src/main/kotlin/org/sayandev/sayanvanish/api/cache/CacheService.kt
@@ -3,6 +3,7 @@ package org.sayandev.sayanvanish.api.cache
 import org.sayandev.sayanvanish.api.User
 import java.util.UUID
 
+// TODO: this is not what i want from a cache system. i want each cache to have its own unique methods. for example i want the getUsers method to have a getUsers().getOnline() sub method. so its probably better to create a seperated class for each type of cache
 interface CacheService {
     fun getUsers(): ICache<UUID, User>
     fun getVanishUsers(): ICache<UUID, User>

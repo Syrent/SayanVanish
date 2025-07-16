fd91d54b210feaef929f977bebcfee3c4c100b7a
Syren <syrent2356@gmail.com>
2025-07-16 15:24:36 +0330

    chore: change default messaging type to websocket

--- sayanvanish-api/src/main/kotlin/org/sayandev/sayanvanish/api/message/MessagingCategoryTypes.kt
+++ sayanvanish-api/src/main/kotlin/org/sayandev/sayanvanish/api/message/MessagingCategoryTypes.kt
@@ -4,7 +4,6 @@ enum class MessagingCategoryTypes(
     override val id: String,
     override val type: MessagingTypes,
 ) : MessagingCategoryType {
-    // TODO: Change default to plugin message or implement WebSocket
-    SYNC_USER("sync_user", MessagingTypes.REDIS),
-    SYNC_VANISH_USER("sync_vanish_user", MessagingTypes.REDIS),
+    SYNC_USER("sync_user", MessagingTypes.WEBSOCKET),
+    SYNC_VANISH_USER("sync_vanish_user", MessagingTypes.WEBSOCKET),
 }

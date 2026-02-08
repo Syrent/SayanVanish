38feb9e471d71e12c1907a9e57808bf5de53b7d1
Syrent <syrent2356@gmail.com>
2026-02-08 08:03:04 +0330

    [ci skip] chore: update stickynote

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
-    id("org.sayandev.stickynote.settings") version "1.10.6.2"
+    id("org.sayandev.stickynote.settings") version "1.10.6.4"
 }
 
 rootProject.name = "SayanVanish"
